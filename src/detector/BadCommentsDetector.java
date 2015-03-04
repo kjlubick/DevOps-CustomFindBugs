0afc42cd872dd8a580139e29df0c82ae76e4bc7b
Kevin Lubick <kjlubick@ncsu.edu>
2015-03-04 14:49:53 -0500

    Clean up

--- src/detector/BadCommentsDetector.java
+++ src/detector/BadCommentsDetector.java
@@ -11,7 +11,6 @@ import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.apache.bcel.classfile.Method;
